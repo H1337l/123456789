8fe2e86b552ea54058d14bd4e6ace5dc348e811a
H1337l <recrentfromme1@gmail.com>
2024-02-03 16:24:59 +0300

    zadanie 1

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print(55)
+f = open('scientist.txt').read()
+f = f.split('\n')
+a = []
+for i in f:
+    a.append(i.split('#'))
+for el in a:
+    if 'Аллопуринол' in el[1]:
+        print('Разработчиками Аллопуринола были такие люди (результаты выведите в порядке возрастания даты):' + el[0])
+x = []
+for el in a:
+    if 'Аллопуринол' in el[1]:
+        x.append(el[2])
+data = min(x)
+for el in a:
+    if data in el[2]:
+        print('Оригинальный рецепт принадлежит:' + el[0])
